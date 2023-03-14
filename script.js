8f2a8ea7529ddd45f2ab713ca87a782a44c5dfbe
Bright <108338051+I3right@users.noreply.github.com>
2023-03-14 14:59:52 +0700

    add comment

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // ------------------ Exercise 1 ---------------------
-// Design an function to calculate pament cost
-// not sure about. How many transection of the mount;
+// Design an function to calculate pament cost not sure about. How many transection of the mount;
 // so add another parameter(freq) for how many transection for that mounth
 function paymentCost(cash, freq) {
   let cost = (3 + cash * 0.01 + cash) * freq;
   let summary = `Should pay ${cost.toFixed(2)}$`;
   return summary;
 }
-// console.log(paymentCost(3, 1));
+console.log(paymentCost(3, 1));
+
 
 // ------------------ Exercise 2 ---------------------
 // part1: get name and log to console
 greeting = (name1, name2, name3) => `Welcome ${name1}, ${name2}, ${name3}`;
 
-// console.log(greeting("A", "B", "C"));
+console.log(greeting("A", "B", "C"));
 
 // part2 : get an age then - with current year
 function ageCalculator(age) {
@@ -21,7 +21,7 @@ function ageCalculator(age) {
   let year = today.getFullYear();
   return Math.abs(year - age);
 }
-// console.log(ageCalculator(1990));
+console.log(ageCalculator(1990)); //assume user age is 1990 can change to promt or input number
 
 // part3
 function greeting2(name1, name2, name3, age1, age2, age3) {
@@ -31,7 +31,8 @@ function greeting2(name1, name2, name3, age1, age2, age3) {
   result += `Welcome ${name3}, you are ${age3} years old. `;
   return result;
 }
-// console.log(greeting2("A", "B", "C", 1, 2, 3));
+console.log(greeting2("A", "B", "C", 1, 2, 3)); // can edit in future to bring from part 2 and part 1 to calculate usefull function
+
 
 // ------------------ Challenge ---------------------
 // Grading 0 -> 11
